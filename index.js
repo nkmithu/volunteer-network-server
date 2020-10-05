581078b01ed34dbdea4f79ec23b4c36f312bb250
nkmithu <nkmithu93@gmail.com>
2020-10-05 23:17:05 +0600

    new

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+const MongoClient = require('mongodb').MongoClient;
+
+console.log(process.env.DB_PASS);
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const port = 5000;
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gaxfg.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+client.connect(err => {
+    const events = client.db("volunteerNetwork").collection("events");
+    const users = client.db("volunteerNetwork").collection("users");
+    // perform actions on the collection object
+    console.log("db connected");
+
+    app.post('/addEvent', (req, res) => {
+        const newEvent = req.body;
+        events.insertOne(newEvent)
+            .then(result => {
+                res.send(result.insertOne > 0);
+            })
+        console.log(newEvent);
+    })
+
+    app.post('/addUser', (req, res) => {
+        const newUser = req.body;
+        users.insertOne(newUser)
+            .then(result => {
+                res.send(result.insertOne > 0);
+            })
+        console.log(newUser);
+    })
+
+    app.get('/events', (req, res) => {
+        events.find({})
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
+
+    app.get('/users', (req, res) => {
+        users.find({})
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
+});
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port)
